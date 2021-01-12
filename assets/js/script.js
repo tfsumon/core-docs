15a16e246bcba3df4a9d861cb9683a78cccec2f1
somratpro <abdulmonnafsomrat@gmail.com>
2021-01-12 12:38:35 +0600

    remove prelaoder

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,3 @@
-// Preloader js  
-function preloader() {
-	$('.preloader').delay(100).fadeOut(10);
-}
-$(preloader);
-$(document).on("turbolinks:load", preloader);
-
 (function ($) {
 	'use strict';
 
