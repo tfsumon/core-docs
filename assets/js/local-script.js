8c18bcdc036d45d90f19366ed32d53a3012f236e
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-01-12 11:50:34 +0700

    theme: add javascript generation pipe

---
+++ assets/js/local-script.js
@@ -0,0 +1,4 @@
+// intentionally empty
+// add a script in your local repo at `assets/js/local-script.js` and
+// add your own scripts to it. This will be added to the scripts pipe
+// AFTER all plugins and AFTER script.js
---
+++ assets/js/variables.js
@@ -0,0 +1,2 @@
+var indexURL = "{{ "index.json" | absLangURL }}";
+var baseurl = "{{ site.BaseURL | safeJS }}{{ if site.IsMultiLingual }}{{ site.LanguagePrefix | safeJS }}{{ end }}";
