c32deabfae4941a560f0ba40eed7e41a478d6b79
somratpro <abdulmonnafsomrat@gmail.com>
2021-08-23 13:34:58 +0600

    change sidebar, fixed preview

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
+// Passive event listeners
+jQuery.event.special.touchstart = {
+	setup: function (_, ns, handle) {
+		'use strict';
+    this.addEventListener('touchstart', handle, {
+      passive: !ns.includes('noPreventDefault')
+    });
+  }
+};
+jQuery.event.special.touchmove = {
+	setup: function (_, ns, handle) {
+		'use strict';
+    this.addEventListener('touchmove', handle, {
+      passive: !ns.includes('noPreventDefault')
+    });
+  }
+};
+
+// Preloader js
+$(window).on('load', function () {
+  'use strict';
+  $('.preloader').fadeOut(0);
+});
+
 (function ($) {
   'use strict';
 
